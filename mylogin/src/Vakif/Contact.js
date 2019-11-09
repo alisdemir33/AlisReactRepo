f81889716774721323ac39553d8c8d19cf60f22e
alisdemir33 <alisdemir33@gmail.com>
2019-11-09 13:05:19 +0300

    tmm

--- mylogin/src/Vakif/Contact.js
+++ mylogin/src/Vakif/Contact.js
@@ -5,6 +5,9 @@ import VakifList from './VakifInfo';
 import VakifForm from './VakifSorguForm';
 import ReactPaginate from 'react-paginate';
 import axios from 'axios';
+import { Button, Container } from 'reactstrap';
+import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Contact extends Component {
 
@@ -13,116 +16,156 @@ class Contact extends Component {
     this.state = {
       vakifList: null,
       ProvinceName: '',
-      foundationName: '',
+      CountyName: '',
       isLoading: false,
-      errorStatus:false
+      errorStatus: false,
+      selectedPage: 0
     }
     this.getVakifListFromServer = this.getVakifListFromServer.bind(this);
   }
 
   pageNumber = 1;
 
+  handlePageClick = data => {
+    debugger;
+    this.pageNumber = data.selected;
+    this.setState({
+      selectedPage: this.pageNumber
+    })
+    this.getVakifListFromServer(this.pageNumber);
+  };
+
   getVakifListFromServer(e) {
+
+    debugger;
 
     let pageSize = 10;
     let ProvinceNameGlob = '';
-    let foundationNameGlob = '';
-    let api_call = null;
-    let response = null;
+    let CountyNameGlob = '';
 
     if (e != null && e != undefined) {
+      ProvinceNameGlob = this.state.ProvinceName;
+      CountyNameGlob = this.state.CountyName;
 
-      e.preventDefault();
+      if (e.target != null) {
+        e.preventDefault();
 
-      if (e.target.elements.PageSize.value != '')
-        pageSize = e.target.elements.PageSize.value;
+        if (e.target.elements.PageSize.value != '')
+          pageSize = e.target.elements.PageSize.value;
 
-      if (e.target.elements.ProvinceName.value != '') {
-        ProvinceNameGlob = e.target.elements.ProvinceName.value;
-      } else {
-        ProvinceNameGlob = this.state.ProvinceName;
+        if (e.target.elements.ProvinceName.value != '') {
+          ProvinceNameGlob = e.target.elements.ProvinceName.value;
+        }
+
+        if (e.target.elements.CountyName.value != '') {
+          CountyNameGlob = e.target.elements.CountyName.value;
+        }
       }
 
-      if (e.target.elements.CountyName.value != '') {
-        foundationNameGlob = e.target.elements.CountyName.value;
-      } else {
-        foundationNameGlob = this.state.foundationName;
-      }
-
-     // this.setState({ isLoading: true });
-
-      console.log(foundationNameGlob, ProvinceNameGlob);
-
-      axios.get(`http://localhost:3993/api/Values/GetVakifListByPagedList?cityFilter=${ProvinceNameGlob}&foundationFilter=${foundationNameGlob}&pageNumber=${this.pageNumber}&pageSize=${pageSize}`)
+      console.log(CountyNameGlob, ProvinceNameGlob);
+      this.setState({ isLoading: true });
+      axios.get(`http://localhost:3993/api/Values/GetVakifListByPagedList?cityFilter=${ProvinceNameGlob}&foundationFilter=${CountyNameGlob}&pageNumber=${this.pageNumber}&pageSize=${pageSize}`)
         .then(response =>
           this.setState({
             vakifList: { PagedListMetaData: response.data.PagedListMetaData, InnerList: response.data.InnerList },
             isLoading: false,
             ProvinceName: ProvinceNameGlob,
-            foundationName: foundationNameGlob,
-            errorStatus:false
+            CountyName: CountyNameGlob,
+            errorStatus: false
           }, () => {
-                //set state call back..
+            //set state call back..
           }))
         .catch(error => this.setState({
-          error:true,
-          isLoading: false
+          errorStatus: true,
+          isLoading: false,
+          vakifList: null
         }));
       console.log(this.state.vakifList);
       console.log(this.state.vakifList);
     }
   }
 
-  handlePageClick = data => {
-    this.pageNumber = data.selected;
-    this.getVakifListFromServer();
-  };
+  removeProvinceFilter = (e) => {
+    this.setState({ ProvinceName: '' });
+  }
+
+  removeCountyFilter = (e) => {
+    this.setState({ CountyName: '' });
+  }
 
   render() {
     const { vakifList } = this.state;
     console.log(vakifList);
+    var pFilter = null, cFilter = null;
 
+    if (this.state.ProvinceName === null || this.state.ProvinceName === '')
+      pFilter = <div />;
+    else {
+      pFilter = <h4><Button close onClick={this.removeProvinceFilter}></Button> Province : {this.state.ProvinceName}</h4>
+    }
+    if (this.state.CountyName === null || this.state.CountyName === '')
+      cFilter = <div />;
+    else {
+      cFilter = <div><Button close onClick={this.removeCountyFilter}></Button><h4> County :{this.state.CountyName}</h4> </div>
+    }
+
+    var commonPart = <div>
+      <h4>SYD Vakıf Listesi Vakıf Sayısı :
+    {vakifList == null ? 0 : this.state.vakifList.PagedListMetaData.RecordCount}</h4>
+      <div>
+        {pFilter}
+        <br />
+        {cFilter}
+      </div>
+      <VakifForm getVakifListFromServerFunction={this.getVakifListFromServer} />
+    </div>
+    
     if (this.state.isLoading === true) {
       return (
-        <h1>Loading..!</h1>
+        <Container className="App">
+          {commonPart}<h1>Loading..!</h1>>
+        </Container>
       );
     }
     else {
 
       if (this.state.errorStatus) {
-        return (
-          <h1>No Data Found..!</h1>
+        return (<Container className="App">
+          {commonPart}<h4>No Data Found..!</h4>
+        </Container>
         );
       }
 
       if (this.state.vakifList != null) {
-      //  debugger;
-        return (
-          <div>
-            <h2>SYD Vakıf Listesi Vakıf Sayısı :
-            {vakifList == null ? 0 : this.state.vakifList.PagedListMetaData.RecordCount}</h2>     
+         return (
+          <div><Container className="App">
+            {commonPart}
 
-            <VakifForm getVakifListFromServerFunction={this.getVakifListFromServer} />
-            <div><h2>Active Filters: {this.state.ProvinceName === null || this.state.ProvinceName === '' ? '' : 'Province Name: ' + this.state.ProvinceName}
-              {this.state.foundationName === null || this.state.foundationName === '' ? '' : '   Foundation Name: ' + this.state.foundationName}  </h2></div>
             <ReactPaginate
               previousLabel={'previous'}
               nextLabel={'next'}
               breakLabel={'...'}
-              breakClassName={'break-me'}
               pageCount={this.state.vakifList == null ? 0 : this.state.vakifList.PagedListMetaData.PageCount}
-              marginPagesDisplayed={2}
-              pageRangeDisplayed={5}
+              marginPagesDisplayed={3}              
+              pageRangeDisplayed={3}
+              forcePage={this.state.selectedPage}
               onPageChange={this.handlePageClick}
+              breakClassName={'page-item'}
+              breakLinkClassName={'page-link'}
               containerClassName={'pagination'}
-              subContainerClassName={'pages pagination'}
+              pageClassName={'page-item'}
+              pageLinkClassName={'page-link'}
+              previousClassName={'page-item'}
+              previousLinkClassName={'page-link'}
+              nextClassName={'page-item'}
+              nextLinkClassName={'page-link'}
               activeClassName={'active'}
             />
 
-            
             <VakifList vakifList={vakifList} />
 
             {/*  <button className="btn btn-info" onClick={this.getVakifListFromServer}> FLİP </button> */}
+          </Container>
           </div>
 
         );
