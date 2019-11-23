5e0611237f298eb52db7216654d8a9dbbdefc9ae
alisdemir33 <alisdemir33@gmail.com>
2019-11-23 09:38:10 +0300

    before  try

--- intro/src/App.js
+++ intro/src/App.js
@@ -5,18 +5,37 @@ import ProductList from "./ProductList";
 import CategoryList from "./CategoryList";
 
 export default class App extends Component {
-  
-    state = { 
-        currentCategory: {categoryId:0 , categoryName : "No Selection"}
-        }
+  state = {
+    currentCategory: { id: 0, categoryName: "No Selection" },
+    products: []
+  };
+  componentDidMount = () => {
+    this.getProducts();
+  }
+
 
   changeCategory = category => {
+   console.log(category);
     this.setState({ currentCategory: category });
+    this.getProducts(category.id)
+  };
+
+  getProducts = (categoryId) => {
+    let url="http://localhost:3000/products";
+    if(categoryId){
+      url=url+"?categoryId="+categoryId;
+    }
+    console.log(url);
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ products: data }))
+      .catch(error => console.log(error));
   };
 
   render() {
-   let productInfo = { title: "ProductList" };
- let  categoryInfo = { title: "CategoryList" };
+    let productInfo = { title: "ProductList" };
+    let categoryInfo = { title: "CategoryList" };
 
     return (
       <div>
@@ -26,10 +45,18 @@ export default class App extends Component {
           </Row>
           <Row>
             <Col xs="3">
-              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
             </Col>
             <Col xs="9">
-              <ProductList currentCategory={this.state.currentCategory} info={productInfo} />
+              <ProductList
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
             </Col>
           </Row>
         </Container>
--- intro/src/ProductList.js
+++ intro/src/ProductList.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
+import { Table } from "reactstrap";
 
 export default class ProductList extends Component {
+  
+  
+  
   render() {
+    let productList = this.props.products.map((item, key) => (
+      <tr key={item.id}>
+        <th scope="row">{key + 1}</th>
+        <td>{item.id}</td>
+        <td>{item.categoryId}</td>
+        <td>{item.productName}</td>
+        <td>{item.quantityPerUnit}</td>
+        <td>{item.unitPrice}</td>
+        <td>{item.unitsInStock}</td>
+      </tr>
+    ));
+
     return (
       <div>
         <h2>Product List</h2>
-        <h2>Current Cat ID : {this.props.currentCategory.categoryId}</h2>
+        <h2>Current Cat ID : {this.props.currentCategory.id}</h2>
         <h2>Current Cat Name: {this.props.currentCategory.categoryName}</h2>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product ID</th>
+              <th>Category ID</th>
+              <th>productName</th>
+              <th>quantityPerUnit</th>
+              <th>Unit Price</th>
+              <th>Units In Stock</th>
+            </tr>
+          </thead>
+          <tbody>{productList}</tbody>
+        </Table>
       </div>
     );
   }
