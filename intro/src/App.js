3af7d57d2b9bfc2008908f77da82f699c9165e2b
alisdemir33 <52675649+alisdemir33@users.noreply.github.com>
2019-11-29 16:27:25 +0300

    CartList added

--- intro/src/App.js
+++ intro/src/App.js
@@ -3,50 +3,53 @@ import { Container, Row, Col } from "reactstrap";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 import CategoryList from "./CategoryList";
+import { Switch, Route } from "react-router-dom";
+import NotFound from "./NotFound";
+import CartList from "./CartList";
 const alertfy = require("alertifyjs");
 
 export default class App extends Component {
   state = {
     currentCategory: { id: 0, categoryName: "No Selection" },
     products: [],
-    cart:[]
+    cart: []
   };
   componentDidMount = () => {
     this.getProducts();
     //alertfy.success('Accepted');
-    
-  }
-
-  addToCart = (product) =>{
-    let newCart= this.state.cart;
-    var addedItem=newCart.find( cart => cart.product.id === product.id);
-    if(addedItem){
-      addedItem.quantity+=1;
-    }else{
-      newCart.push({product:product,quantity:1});  
-    }    
-    this.setState({cart:newCart});
-    alertfy.success(product.productName +' Added!')
-  }
-
-  removeFromCart = (product) =>{
-    let newCart = this.state.cart.filter( item => item.product.id !== product.id);
-    console.log(newCart);
-    this.setState({cart:newCart});
-    alertfy.error(product.productName +' Removed!')
-  }
-
-
-  changeCategory = category => {
-   console.log(category);
-    this.setState({ currentCategory: category });
-    this.getProducts(category.id)
   };
 
-  getProducts = (categoryId) => {
-    let url="http://localhost:3000/products";
-    if(categoryId){
-      url=url+"?categoryId="+categoryId;
+  addToCart = product => {
+    let newCart = this.state.cart;
+    var addedItem = newCart.find(cart => cart.product.id === product.id);
+    if (addedItem) {
+      addedItem.quantity += 1;
+    } else {
+      newCart.push({ product: product, quantity: 1 });
+    }
+    this.setState({ cart: newCart });
+    alertfy.success(product.productName + " Added!");
+  };
+
+  removeFromCart = product => {
+    let newCart = this.state.cart.filter(
+      item => item.product.id !== product.id
+    );
+    console.log(newCart);
+    this.setState({ cart: newCart });
+    alertfy.error(product.productName + " Removed!");
+  };
+
+  changeCategory = category => {
+    console.log(category);
+    this.setState({ currentCategory: category });
+    this.getProducts(category.id);
+  };
+
+  getProducts = categoryId => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url = url + "?categoryId=" + categoryId;
     }
     console.log(url);
 
@@ -63,9 +66,11 @@ export default class App extends Component {
     return (
       <div>
         <Container>
-         
-            <Navi removeFromCart={this.removeFromCart} cart={this.state.cart}></Navi>
-         
+          <Navi
+            removeFromCart={this.removeFromCart}
+            cart={this.state.cart}
+          ></Navi>
+
           <Row>
             <Col xs="3">
               <CategoryList
@@ -75,12 +80,23 @@ export default class App extends Component {
               />
             </Col>
             <Col xs="9">
-              <ProductList
-                products={this.state.products}
-                addToCart={this.addToCart}
-                currentCategory={this.state.currentCategory}
-                info={productInfo}
-              />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={props => (
+                    <ProductList
+                      {...props}
+                      products={this.state.products}
+                      addToCart={this.addToCart}
+                      currentCategory={this.state.currentCategory}
+                      info={productInfo}
+                    />
+                  )}
+                />
+                <Route exact path="/cart" component={CartList} />
+                <Route exact path="/" component={NotFound} />>
+              </Switch>
             </Col>
           </Row>
         </Container>
