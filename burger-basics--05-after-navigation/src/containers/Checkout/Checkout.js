4dd713532069a56c256f3e3abf39ca3a85b991fe
alisdemir33 <alisdemir33@gmail.com>
2020-03-05 07:44:49 +0300

    contactdata form added

--- burger-basics--05-after-navigation/src/containers/Checkout/Checkout.js
+++ burger-basics--05-after-navigation/src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import {Route} from 'react-router-dom'
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from '../Checkout/ContactData/ContactData'
 
 class Checkout extends Component {
   state = {
@@ -45,6 +47,8 @@ class Checkout extends Component {
           checkOutContinued={this.checkOutContinuedClicked}
           ingredients={this.state.ingredients}
         />
+        <Route path={this.props.match.url+'/contact-data'} component={ContactData} ></Route>
+
       </div>
     );
   }
--- burger-basics--05-after-navigation/src/containers/Checkout/ContactData/ContactData.js
+++ burger-basics--05-after-navigation/src/containers/Checkout/ContactData/ContactData.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Button from '../../../components/UI/Button/Button'
+import classes from './ContactData.css'
 
- class ContactData extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+class ContactData extends Component {
+  state = {
+    name: "",
+    email: "",
+    address: {
+      street: "",
+      postalCode: ""
     }
+  };
+
+  render() {
+    return (
+    <div className={classes.ContactData}>
+        <h4>Enter Your Contact Data Please!</h4>
+        <form>
+            <input type="Text" name="Name" placeholder="Your Name"></input>
+            <input type="Email" name="Email" placeholder="Your Email"></input>
+            <input type="Text" name="Street" placeholder="Street Name"></input>
+            <input type="Text" name="PostalCode" placeholder="Postal Code"></input>
+            <Button btnType='SUCCESS'>ORDER</Button>
+        </form>
+    </div>
+    );
+  }
 }
 
 export default ContactData;
