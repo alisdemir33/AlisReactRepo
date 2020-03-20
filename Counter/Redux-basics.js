2bd5547f136de42461bcec7d3c8e367c1f3ae50f
alisdemir33 <52675649+alisdemir33@users.noreply.github.com>
2020-03-20 17:32:27 +0300

    redux

--- Counter/Redux-basics.js
+++ Counter/Redux-basics.js
@@ -1,16 +1,40 @@
-const redux = require('redux')
+const redux = require("redux");
 
+const createStore = redux.createStore;
 
-const createStore =redux.createStore;
-
-const initialState={
-    counter:1
-}
+const initialState = {
+  counter: 1
+};
 //reducer
-const rootReducer=(state=initialState,action) =>{
-    return state;
+const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
+  return state;
 };
 
 //store
 const store = createStore(rootReducer);
-console.log(store.getState());
+console.log('after store created', store.getState());
+
+store.subscribe( () => {
+    console.log('[Inside Subscription]' , store.getState());
+})
+
+console.log('After subscribe to store' , store.getState());
+
+store.dispatch({ type: "INC_COUNTER" });
+store.dispatch({ type: "ADD_COUNTER", value: 5 });
+
+console.log('After Dispatch!' , store.getState());
+
