a1e168a5c8fc289315f1e8d52ced2e8cdf6645e4
ALİ DEMİRÖZ <ali.demiroz@ailevecalisma.gov.tr>
2020-03-30 00:11:11 +0300

    counter delete onclick added

--- Counter/src/containers/Counter/Counter.js
+++ Counter/src/containers/Counter/Counter.js
@@ -5,60 +5,79 @@ import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-            default:
-                this.setState( ( prevState ) => { return { counter: prevState.counter } } )
-                    
+    /*     state = {
+            counter: 0       
         }
-    }
+    
+        counterChangedHandler = ( action, value ) => {
+            switch ( action ) {
+                case 'inc':
+                    this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+                    break;
+                case 'dec':
+                    this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+                    break;
+                case 'add':
+                    this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+                    break;
+                case 'sub':
+                    this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+                    break;
+                default:
+                    this.setState( ( prevState ) => { return { counter: prevState.counter } } )
+                        
+            }
+        } */
 
-    render () {
+    render() {
+        let retVal = <li>Empty</li>
+        console.log(this.props.storedResults)
+        
+        if (this.props.storedResults != null)
+
+            retVal = this.props.storedResults.map((item, index) => {
+                return <li onClick={() => this.props.onDeleteItem(item.id)} key={item.id} > {item.value}</li>
+            });
+
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this. props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={this. props.onAddFive}  />
-                <CounterControl label="Subtract 5" clicked={this. props.onSubtractFive}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 5" clicked={this.props.onAddFive} />
+                <CounterControl label="Subtract 5" clicked={this.props.onDeleteItem} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {
+                        retVal
+                    }
+                </ul>
             </div>
         );
     }
 }
 
-const mapDispatchToProps = (dispatch) =>{
-
-    return{
-        onIncrementCounter: () => { dispatch({type:'INCREMENT'})},
-        onDecrementCounter :() => {dispatch({type:'DECREMENT'})},
-        onAddFive :() => {dispatch({type:'ADDFIVE',payload:3})},
-        onSubtractFive :() => {dispatch({type:'SUBFIVE',payload:4})}
-    }
-
-}
-
 const mapStateToProps = state => {
-
+    console.log(state.results)
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 
 };
+
+const mapDispatchToProps = (dispatch) => {
+
+    return {
+        onIncrementCounter: () => { dispatch({ type: 'INCREMENT' }) },
+        onDecrementCounter: () => { dispatch({ type: 'DECREMENT' }) },
+        onAddFive: () => { dispatch({ type: 'ADDFIVE', payload: 3 }) },
+        onSubtractFive: () => { dispatch({ type: 'SUBFIVE', payload: 4 }) },
+        onStoreResult: () => { dispatch({ type: 'STORE' }) },
+        onDeleteItem: (itemValue) => { dispatch({ type: 'DELETE', payload: itemValue }) }
+    }
+
+}
 //mapStateToProps yok ise null gönderiyoruz
-export default connect(mapStateToProps,mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- Counter/src/store/reducer.js
+++ Counter/src/store/reducer.js
@@ -1,29 +1,53 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []//empty arry definition
 };
 
 const reducer = (state = initialState, action) => {
+  console.log(action);
   switch (action.type) {
     case "INCREMENT":
       return {
+        ...state,
         counter: state.counter + 1
       };
     case "DECREMENT":
       return {
+        ...state,
         counter: state.counter - 1
       };
     case "ADDFIVE":
       return {
+        ...state,
         counter: state.counter + action.payload
       };
     case "SUBFIVE":
       return {
+        ...state,
         counter: state.counter - action.payload
       };
-    default:
+    case "STORE":
       return {
-        counter:state.counter
+        ...state,
+        results: [...state.results, { id: new Date(), value: state.counter }]
       };
+
+    case "DELETE":
+      {
+
+        return {
+          ...state,
+          results: state.results.filter((item, index) => {
+            if (item.id === action.payload)
+              return false;
+            else
+              return true;
+          })
+        };
+      }
+
+    default:
+      return state;
   }
 };
 
