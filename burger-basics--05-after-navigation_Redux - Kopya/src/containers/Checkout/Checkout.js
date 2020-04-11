8a64832dbc65919202c3d442909bd32ccb0dd851
ALİ DEMİRÖZ <ali.demiroz@ailevecalisma.gov.tr>
2020-04-11 22:42:32 +0300

    before we go..redux

---
+++ burger-basics--05-after-navigation_Redux - Kopya/src/containers/Checkout/Checkout.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import ContactData from "../Checkout/ContactData/ContactData";
+
+class Checkout extends Component {
+  state = {
+    ingredients: null
+  /*   {
+      salad: 1,
+      meat: 1,
+      bacon: 1,
+      cheese: 1
+    } */
+    ,
+    price: 0
+  };
+
+  componentWillMount() {
+    //  console.log()
+    /*  this.setState({
+            ingredients:this.props.location.state.currentIngredients
+        }) */
+
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    let totPrice = 0;
+    for (let param of query.entries()) {
+      // ['salad', '1']
+      if (param[0] === "price") {
+        totPrice = +param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
+    }
+
+    this.setState({
+      ingredients: ingredients,
+      price: totPrice.toFixed(2)
+    });
+  }
+
+  checkOutCanceledClicked = () => {
+    console.log("Cancel Clicked!!");
+    this.props.history.goBack();
+  };
+
+  checkOutContinuedClicked = () => {
+    console.log("Continue Clicked!!");
+    this.props.history.replace("/checkout/contact-data");
+  };
+
+  render() {
+    return (
+      <div>
+        <CheckoutSummary
+          checkOutCanceled={this.checkOutCanceledClicked}
+          checkOutContinued={this.checkOutContinuedClicked}
+          ingredients={this.state.ingredients}
+        />
+        <Route
+          path={this.props.match.url + "/contact-data"}
+          render={(props) => (
+            <ContactData
+              ingredients={this.state.ingredients}
+              totalPrice={this.state.price}
+              {...props}
+            />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export default Checkout;
---
+++ burger-basics--05-after-navigation_Redux - Kopya/src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,227 @@
+import React, { Component } from "react";
+import Button from "../../../components/UI/Button/Button";
+import classes from "./ContactData.css";
+import axios from "../../../axios-orders";
+import Spinner from "../../../components/UI/Spinner/Spinner";
+import Input from "../../../components/UI/Input/Input";
+
+class ContactData extends Component {
+  state = {
+    ingredients: null,
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "your name"
+        },
+        value: "",
+        touched: false,
+        validation: {
+          required: true          
+        },
+        valid: false
+      },
+
+      street: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "street"
+        },
+        value: "",
+        touched: false,
+        validation: {
+          required: true
+        },
+        valid: false
+      },
+
+      zipCode: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Zip Code"
+        },
+        value: "",
+        touched: false,
+        validation: {
+          required: true,
+          minlength: 5,
+          maxlength: 5
+        },
+        valid: false
+      },
+
+      country: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Country"
+        },
+        value: "",
+        touched: false,
+        validation: {
+          required: true
+        },
+        valid: false
+      },
+      email: {
+        elementType: "email",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Email"
+        },
+        value: "",
+        touched: false,
+        validation: {
+          required: true
+        },
+        valid: false
+      },
+
+      deliveryMethod: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            { value: "fastest", displayValue: "Fastest" },
+            { value: "cheapest", displayValue: "Cheapest" }
+          ]
+        },
+        value: 'fastest',
+        validation:{},
+        valid:true
+      }
+    },
+    formIsValid:false,
+    price: 5,
+    loading: false
+  };
+
+  //let ingredients;
+
+  componentDidMount() {
+    this.setState({
+      ingredients: this.props.ingredients,
+      price: this.props.totalPrice
+    });
+  }
+
+  checkValidity(value, validation) {
+    let isValid = true;
+
+    if (validation) {
+      if (validation.required) {
+        isValid = value.trim() !== "" && isValid;
+      }
+      if (validation.minlength) {
+        isValid = value.length >= validation.minlength && isValid;
+      }
+      if (validation.maxlength) {
+        isValid = value.length <= validation.maxlength && isValid;
+      }
+    }
+    return isValid;
+  }
+
+  orderHandler = event => {
+    event.preventDefault();
+    console.log(this.state.totalPrice);
+
+    this.setState({ loading: true });
+
+    const formData = {};
+
+    for (let formElemIdentifier in this.state.orderForm) {
+      formData[formElemIdentifier] = this.state.orderForm[
+        formElemIdentifier
+      ].value;
+    }
+
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.price,
+      orderData: formData
+    };
+
+    axios
+      .post("/orders.json", order)
+      .then(response => {
+        this.setState({ loading: false }, () => {
+          this.props.history.push("/");
+        });
+
+        alert("Order is accepted!");
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
+  };
+
+  inputChangedHandler = (event, inputIdentifier) => {
+    console.log(event.target.value);
+    const orderFormCopy = { ...this.state.orderForm };
+    const clonedFormElement = { ...orderFormCopy[inputIdentifier] };
+    clonedFormElement.value = event.target.value;
+    clonedFormElement.touched = true;
+    clonedFormElement.valid = this.checkValidity(
+      event.target.value,
+      clonedFormElement.validation
+    );
+    orderFormCopy[inputIdentifier] = clonedFormElement;
+   
+    let formIsValid=true;
+    for(let identifier in orderFormCopy){
+     if(! orderFormCopy[identifier].valid){
+      formIsValid=false;
+     }
+    }
+
+    console.log(formIsValid);
+
+    this.setState({ orderForm: orderFormCopy, formIsValid:formIsValid });
+  };
+
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
+    const formWithElements = formElementsArray.map(item => {
+      return (
+        <Input
+          key={item.id}
+          invalid={!item.config.valid}
+          touched={item.config.touched}
+          elementType={item.config.elementType}
+          elementConfig={item.config.elementConfig}
+          customMsg={item.config.elementConfig.placeholder}
+          value={item.config.value}
+          shouldValidate={item.config.validation && item.config.touched}
+          changed={event => this.inputChangedHandler(event, item.id)}
+        ></Input>
+      );
+    });
+
+    let form = (
+      <form onSubmit={this.orderHandler}>
+        {formWithElements}
+        <Button btnType="Success" disabled ={!this.state.formIsValid}>ORDER</Button>
+      </form>
+    );
+
+    if (this.state.loading) form = <Spinner></Spinner>;
+
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter Your Contact Data Please!</h4>
+        {form}
+      </div>
+    );
+  }
+}
+
+export default ContactData;
