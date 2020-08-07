c4d9b27c6420f5478056f51e855710c32555955c
alisdemir33 <52675649+alisdemir33@users.noreply.github.com>
2020-08-07 17:47:46 +0300

    refreshtoken

--- burger-basics--05-after-navigation_Redux_jwt/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-basics--05-after-navigation_Redux_jwt/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,49 +1,98 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Modal from '../../components/UI/Modal/Modal';
-import Aux from '../AuxFolder/Auxi';
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from "../AuxFolder/Auxi";
 
-const withErrorHandler = ( WrappedComponent, axios ) => {
-    return class extends Component {
-        state = {
-            error: null
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null,
+    };
+
+    componentWillMount() {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        return req;
+      });
+      this.resInterceptor = axios.interceptors.response.use(
+        (res) => res,
+        (error) => {
+          
+          this.setState({ error: error });
+         
+          ;debugger
+          return new Promise((resolve, reject) => {
+            const originalReq = error.config;
+            if (
+              error.response.status === 401 &&
+              error.config &&
+              !error.config.__isRetryRequest
+            ) {
+              originalReq._retry = true;
+
+              let res = fetch("https://localhost:44384/sample/refreshToken", {
+                method: "POST",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "same-origin",
+                headers: {
+                  "Content-Type": "application/json",
+                  Device: "device",
+                  Token: localStorage.getItem("token").accessToken,
+                },
+                redirect: "follow",
+                referrer: "no-referrer",
+                body: JSON.stringify({
+                  token: localStorage.getItem("token"),
+                  refreshToken: localStorage.getItem("token").refreshToken,
+                }),
+              })
+                .then((res) => res.json())
+                .then((res) => {
+                  console.log(res);
+                  this.setSession({
+                    token: res.token,
+                    refreshToken: res.refresh,
+                  });
+                  originalReq.headers["Token"] = res.token;
+                  originalReq.headers["Device"] = "device";
+
+                  return axios(originalReq);
+                });
+
+              resolve(res);
+            }
+
+            return Promise.reject(error);
+          });
         }
-
-        componentWillMount () {
-            this.reqInterceptor = axios.interceptors.request.use( req => {
-                this.setState( { error: null } );
-                return req;
-            } );
-            this.resInterceptor = axios.interceptors.response.use( res => res, 
-                error => {
-                this.setState( { error: error } );
-                    
-
-            } );
-        }
-
-        componentWillUnmount () {
-            axios.interceptors.request.eject( this.reqInterceptor );
-            axios.interceptors.response.eject( this.resInterceptor );
-        }
-
-        errorConfirmedHandler = () => {
-            this.setState( { error: null } );
-        }
-
-        render () {
-            return (
-                <Aux>
-                    <Modal
-                        show={this.state.error}
-                        modalClosed={this.errorConfirmedHandler}>
-                        {this.state.error ? this.state.error.message : null}
-                    </Modal>
-                    <WrappedComponent {...this.props} />
-                </Aux>
-            );
-        }
+      );
     }
-}
+
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+
+    render() {
+      return (
+        <Aux>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      );
+    }
+  };
+};
 
 export default withErrorHandler;
